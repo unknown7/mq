99445996d8673cb5546b8cb88e6250aaf1a1738d
unknown7 <unknown7@live.cn>
2019-04-19 15:01:29 +0800

    用户表、订单表

---
+++ src/main/java/com/mq/model/Order.java
@@ -0,0 +1,106 @@
+package com.mq.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Order {
+    private Long id;
+
+    private Long goodsId;
+
+    private String goodsType;
+
+    private BigDecimal goodsPrice;
+
+    private Long userId;
+
+    private BigDecimal accountBalanceAmount;
+
+    private BigDecimal wxAmount;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private Integer delFlag;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(Long goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public String getGoodsType() {
+        return goodsType;
+    }
+
+    public void setGoodsType(String goodsType) {
+        this.goodsType = goodsType == null ? null : goodsType.trim();
+    }
+
+    public BigDecimal getGoodsPrice() {
+        return goodsPrice;
+    }
+
+    public void setGoodsPrice(BigDecimal goodsPrice) {
+        this.goodsPrice = goodsPrice;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public BigDecimal getAccountBalanceAmount() {
+        return accountBalanceAmount;
+    }
+
+    public void setAccountBalanceAmount(BigDecimal accountBalanceAmount) {
+        this.accountBalanceAmount = accountBalanceAmount;
+    }
+
+    public BigDecimal getWxAmount() {
+        return wxAmount;
+    }
+
+    public void setWxAmount(BigDecimal wxAmount) {
+        this.wxAmount = wxAmount;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Integer getDelFlag() {
+        return delFlag;
+    }
+
+    public void setDelFlag(Integer delFlag) {
+        this.delFlag = delFlag;
+    }
+}
---
+++ src/main/java/com/mq/model/User.java
@@ -0,0 +1,145 @@
+package com.mq.model;
+
+import java.util.Date;
+
+public class User {
+    private Long id;
+
+    private String country;
+
+    private Integer gender;
+
+    private String province;
+
+    private String city;
+
+    private String avatarurl;
+
+    private String openid;
+
+    private String nickname;
+
+    private String language;
+
+    private String sessionKey;
+
+    private String skey;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private Integer delFlag;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country == null ? null : country.trim();
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province == null ? null : province.trim();
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city == null ? null : city.trim();
+    }
+
+    public String getAvatarurl() {
+        return avatarurl;
+    }
+
+    public void setAvatarurl(String avatarurl) {
+        this.avatarurl = avatarurl == null ? null : avatarurl.trim();
+    }
+
+    public String getOpenid() {
+        return openid;
+    }
+
+    public void setOpenid(String openid) {
+        this.openid = openid == null ? null : openid.trim();
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname == null ? null : nickname.trim();
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language == null ? null : language.trim();
+    }
+
+    public String getSessionKey() {
+        return sessionKey;
+    }
+
+    public void setSessionKey(String sessionKey) {
+        this.sessionKey = sessionKey == null ? null : sessionKey.trim();
+    }
+
+    public String getSkey() {
+        return skey;
+    }
+
+    public void setSkey(String skey) {
+        this.skey = skey == null ? null : skey.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Integer getDelFlag() {
+        return delFlag;
+    }
+
+    public void setDelFlag(Integer delFlag) {
+        this.delFlag = delFlag;
+    }
+}
