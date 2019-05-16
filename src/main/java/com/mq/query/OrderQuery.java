f5a54aec7a65a1fe8a4efea1b23fcc7fcaa3cf4e
unknown7 <unknown7@live.cn>
2019-05-16 22:44:27 +0800

    portal
    Signed-off-by: unknown7 <unknown7@live.cn>

--- src/main/java/com/mq/query/OrderQuery.java
+++ src/main/java/com/mq/query/OrderQuery.java
@@ -5,7 +5,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import java.math.BigDecimal;
 import java.util.Date;
 
-public class OrderQuery {
+public class OrderQuery extends DefaultQuery {
     private Long id;
 
     private String orderNo;
