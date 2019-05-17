0066144becc1451d731bec416dd43fd1042cbe83
unknown7 <unknown7@live.cn>
2019-05-18 03:54:19 +0800

    white list
    Signed-off-by: unknown7 <unknown7@live.cn>

---
+++ src/main/java/com/mq/model/WhiteList.java
@@ -0,0 +1,33 @@
+package com.mq.model;
+
+public class WhiteList {
+    private Long id;
+
+    private Long userId;
+
+    private String skey;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getSkey() {
+        return skey;
+    }
+
+    public void setSkey(String skey) {
+        this.skey = skey == null ? null : skey.trim();
+    }
+}
