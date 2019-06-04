a2605bc039cedcaec04efa758b854126ede8a199
unknown7 <unknown7@live.cn>
2019-06-04 13:52:52 +0800

    minor revision
    Signed-off-by: unknown7 <unknown7@live.cn>

--- src/main/java/com/mq/SpringBootStartApplication.java
+++ src/main/java/com/mq/SpringBootStartApplication.java
@@ -1,15 +1,10 @@
 package com.mq;
 
-import com.github.pagehelper.autoconfigure.PageHelperAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 @MapperScan("com.mq.mapper")
